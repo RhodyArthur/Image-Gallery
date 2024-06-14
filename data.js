9a875a4cf00f460b3a5ea2359450da4b10a1035e
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2024-06-14 04:48:58 -0700

    create data file

---
+++ data.js
@@ -0,0 +1,51 @@
+export const sportsGallery = [
+{img:'images/serenawilliams.jpeg',
+caption:'Serena Williams serves during her third-round match against Qiang Wang in the Australian Open on Jan. 24.',
+'name':'Serena Williams',
+thumbnail_url: 1},
+
+{img:'images/cristiano-ronaldo.jpeg',
+caption:'Cristiano Ronaldo scores last-minute winner on record-breaking 200th appearance for Portugal',
+'name':'Cristiano Ronaldo',
+thumbnail_url: 2},
+
+{img:'images/lionel-messi.jpeg',
+caption:'Lionel Messi and defending champion Argentina still hungry for titles ahead of Copa America',
+name:'Lionel Messi',
+thumbnail_url: 3},
+
+{img:'images/mbappe.jpeg',
+caption:'Kylian Mbappé Tells P.S.G. He Won’t Extend Contract in 2024',
+name:'Kylian Mbappe',
+thumbnail_url: 4},
+
+{img:'images/jude.jpeg',
+caption:'A 20-year-old Englishman fueled Real Madrid’s record-extending La Liga title and became one of the world’s best players',
+name:'Jude Bellingham',
+thumbnail_url: 5},
+
+{img:'images/alex.jpeg',
+caption:'Washington QB Alex Smith will take time to ponder NFL future',
+name:'Alex Smith',
+thumbnail_url: 6},
+
+{img:'images/lebron.jpeg',
+caption:'LeBron James considering retirement after Los Angeles Lakers swept by Denver Nuggets',
+name:'Lebron James',
+thumbnail_url: 7},
+
+{img:'images/reece.jpeg',
+caption:'Reece James and sister Lauren aim for new FA Cup record with Chelsea this weekend',
+name:'James and Lauren Reece',
+thumbnail_url: 8},
+
+{img:'images/tennis.jpeg',
+caption:'Best Current Male Tennis Player',
+name:'Roger Federer',
+thumbnail_url: 9},
+
+{img:'images/athlete.jpeg',
+caption:'Tokyo 2020: Joseph Amoah qualifies for semi-final after finishing 3rd in men’s 200m heat',
+name:'Joseph Paul Amoah',
+thumbnail_url: 10}
+]
